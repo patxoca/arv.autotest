db3f44ba6a5af237966bf2906a21eb9428472156
alex <alex@6fc8c6bf-3316-0410-aa01-e414f6f0c4bf>
2014-04-19 17:45:31 +0000

    En les proves fetes en el propi 'autotest' s'ha detectat que resulta dificil determinar si el script s'ha executat (la sortida generada per execucions consecutives tendeix a ser la mateixa).
    Per mirar de resoldre-ho s'ha afegit un comptador d'execució i la data
    i hora a la "barra" informativa.
    
    
    git-svn-id: file:///tmp/svn/repo/prova/autotest/trunk@5 6fc8c6bf-3316-0410-aa01-e414f6f0c4bf

--- autotest/reactor.py
+++ autotest/reactor.py
@@ -2,15 +2,33 @@
 
 # $Id$
 
+from datetime import datetime
+
 from blessings import Terminal
 
 
 t = Terminal()
+counter = 0
 
 def react(code, output):
+    """Displays the outcome.
+
+    Displays the ``output`` variable. If ``code`` is zero (no error)
+    displays an 'OK' message on green otherwise an 'ERROR' message on
+    red.
+
+    """
+    global counter
+    counter += 1
+    if code:
+        formatter = t.bold_white_on_red
+        message = "ERROR"
+    else:
+        formatter = t.bold_white_on_green
+        message = "OK"
+    stamp = "%3i " % counter + datetime.now().strftime("%H:%M:%S %d/%m/%Y")
+    message = message.center(t.width - 1 - len(stamp), " ")
+
     print output
     print
-    if code:
-        print t.bold_white_on_red("ERROR".center(79, " "))
-    else:
-        print t.bold_white_on_green("OK".center(79, " "))
+    print formatter(stamp + message)
