46c60245d5a40c5b40dfb83a978a099bab6ddace
alex <alex@6fc8c6bf-3316-0410-aa01-e414f6f0c4bf>
2014-04-20 19:00:24 +0000

    S'ajusta el mòdul 'main.py' als canvis fets en les últimes revisions.
    git-svn-id: file:///tmp/svn/repo/prova/autotest/trunk@16 6fc8c6bf-3316-0410-aa01-e414f6f0c4bf

--- autotest/main.py
+++ autotest/main.py
@@ -7,12 +7,13 @@ import pyinotify
 from autotest import config
 from autotest import reactor
 from autotest import runner
-from autotest import walker
+from autotest import filters
 
 
 class EventHandler(pyinotify.ProcessEvent):
-    def my_init(self, callback):
+    def my_init(self, callback, filter):
         self._callback = callback
+        self._filter = filter
 
     # useful to understand how pyinotify works. If you uncomment this
     # method you should use a mask 'pyinotify.ALL_EVENTS' in order to
@@ -21,19 +22,38 @@ class EventHandler(pyinotify.ProcessEvent):
     # def process_default(self, event):
     #     print str(event)
 
+    def process_IN_DELETE(self, event):
+        if not self._filter(event):
+            return
+        self._callback()
+
     def process_IN_CLOSE_WRITE(self, event):
+        if not self._filter(event):
+            return
         self._callback()
 
 
 def main():
-    cfg = config.read_config(None)
-    files = walker.get_file_list(None)
+    cfg = config.read_config("autotest.cfg")
     def callback():
         code, output = runner.run(cfg.command)
         reactor.react(code, output)
     wm = pyinotify.WatchManager()
-    notifier = pyinotify.Notifier(wm, EventHandler(callback=callback))
-    wm.add_watch(files, pyinotify.IN_CLOSE_WRITE)
+    handler = EventHandler(
+        callback=callback,
+        filter=filters.and_(
+            filters.not_(filters.is_delete_dir_event),
+            filters.simple_event_filter_factory(cfg.watch, cfg.global_ignore)
+        )
+    )
+    notifier = pyinotify.Notifier(wm, handler)
+    for watch in cfg.watch:
+        wm.add_watch(
+            watch.path,
+            pyinotify.IN_CLOSE_WRITE|pyinotify.IN_DELETE,
+            rec=watch.recurse,
+            auto_add=watch.auto_add
+        )
     notifier.loop()
 
 main()
